ecf7e65d9aef4c95d97466a21adeee3d4b8a8ab3
Fᴀʏᴀs.N <73426323+FayasKKD@users.noreply.github.com>
2020-12-03 16:50:24 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -37,10 +37,7 @@ All commands can be used with the following: / !
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "If you have any bugs or questions on how to use me, have a look at my [Channel](https://t.me/ProIndians).")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+Please Donate in my Donation Site."""
 
 IMPORTED = {}
 MIGRATEABLE = []
